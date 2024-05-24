67b952d464d585861ceeca2211b996ee888d45e5
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2024-05-24 10:21:34 +0530

    Add files via upload

---
+++ cgpa-sgpa/CGPA/script.js
@@ -0,0 +1,50 @@
+const submit = document.querySelector('#submit');
+submit.setAttribute('class','submit-class');
+var num = 1;
+const button = document.createElement('button');
+button.setAttribute('class','submit-class');
+button.innerText = "Calculate";
+button.setAttribute('id','btn');
+
+let arr = [];
+
+submit.addEventListener('click',()=>{
+  num = semester.value;
+  addFields(num);
+
+})
+
+function addFields(num){
+  const len = num
+  for(let i=0;i<num;i++){
+    const newDiv = document.createElement('div');
+    newDiv.innerHTML = '<input class="sgpa" type="number" placeholder="SGPA"> <input class="credits" type="number" min="1" placeholder="Credits">'
+    newDiv.classList.add('semester');
+    document.querySelector('#details').append(newDiv);
+  }
+  document.querySelector('#details').append(button);
+  document.querySelector('#details').style.display = 'block';
+
+}
+
+button.addEventListener('click',()=>{
+  const credits = document.querySelectorAll('.credits');
+  const sgpa = document.querySelectorAll('.sgpa');
+  for(let i=0;i<num;i++){
+    arr.push([parseFloat(sgpa[i].value), parseInt(credits[i].value)]);
+  }
+  findCGPA();
+})
+
+function findCGPA(){
+  var totalCredits = 0;
+  var totalPoints = 0;
+  for(let i=0; i<num; i++){
+    totalCredits += arr[i][1];
+    totalPoints += arr[i][0] * arr[i][1];
+  }
+  var cgpa = totalPoints / totalCredits;
+  const result = document.querySelector('#result');
+  result.innerHTML = "Your CGPA is: " + cgpa.toFixed(2);
+  result.style.display = 'block';
+}
